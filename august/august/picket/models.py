4cc583fc04aa7c7ca71e008c8a46a591a4fb1ad1
onlyphantom <samuel@outlook.sg>
2021-07-30 13:42:45 +0700

    first migration

--- august/august/picket/models.py
+++ august/august/picket/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    email = models.EmailField(max_length=254, unique=True)
+    name = models.CharField(max_length=255)
+    joined_at = models.DateTimeField(auto_now_add=True)
+    number_of_lessons = models.PositiveSmallIntegerField(default=0)
+
+class Kelas(models.Model):
+    name = models.CharField(max_length=255)
+class Class_Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    kelas = models.ForeignKey(Kelas, on_delete=models.CASCADE)
+
--- august/august/picket/urls.py
+++ august/august/picket/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from . import views
+# from . import views
+from picket.views import home, signup
 
 urlpatterns = [
-    path('api', views.home, name='index'),
-    path('signup', views.signup, name='signup')
+    path('api', home, name='index'),
+    path('signup', signup, name='signup'),
 ]
