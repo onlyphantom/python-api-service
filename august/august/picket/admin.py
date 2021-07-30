e1d0bb14054da78dfa1a366020b14a3052ba7fd1
onlyphantom <samuel@outlook.sg>
2021-07-30 14:27:41 +0700

    create Student, Kelas, Class_Enrollment

--- august/august/picket/admin.py
+++ august/august/picket/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student, Kelas, Class_Enrollment
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Kelas)
+admin.site.register(Class_Enrollment)
+
